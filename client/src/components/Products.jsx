5b925f6b639742800c7cd0dad771da1784383f15
Maayan Bitan <maayanb578@gmail.com>
2022-06-20 10:07:37 +0300

    new commit

--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -12,28 +12,38 @@ const Container = styled.div`
     justify-content: space-between;
 `;
 
-const Products = ({cat,filters,sort}) => {
-  
+const Products = ({ cat, filters, sort }) => {
+
   const [products, setproducts] = useState([]);
   const [filteredProducts, setFilteredProducts] = useState([]);
 
   useEffect(() => {
-    const getProducts = async ()=>{
-      try{
-        const res = await axios.get("http://localhost:5000/api/products");
-        console.log(res.data)
-      }catch(err){
+    const getProducts = async () => {
+      try {
+        const res = await axios.get(cat ? `http://localhost:5000/api/products?category=${cat}` : "http://localhost:5000/api/products");
+        setproducts(res.data);
+        console.log(res.data);
+      } catch (err) {
 
       }
     };
     getProducts()
-   
+
   }, [cat]);
+
+  useEffect(() => {
+    cat && setFilteredProducts(
+      products.filter((item) => Object.entries(filters).every(([key, value]) =>
+        item[key].includes(value)
+      ))
+    )
+  }, [products, cat, filters]);
+  
   return (
     <Container>
-     {popularProducts.map((item)=>(
-     <Product item={item} key={item.id} />
-     ))}
+      {filteredProducts.map((item) => (
+        <Product item={item} key={item.id} />
+      ))}
     </Container>
   )
 }
--- client/src/pages/ProductList.jsx
+++ client/src/pages/ProductList.jsx
@@ -73,12 +73,13 @@ const ProductList = () => {
                         <Option disabled >
                             Color
                         </Option>
-                        <Option>White</Option>
-                        <Option>Black</Option>
-                        <Option>Red</Option>
-                        <Option>Blue</Option>
-                        <Option>Yellow</Option>
-                        <Option>Green</Option>
+                        <Option>white</Option>
+                        <Option>gray</Option>
+                        <Option>black</Option>
+                        <Option>red</Option>
+                        <Option>blue</Option>
+                        <Option>yellow</Option>
+                        <Option>green</Option>
                     </Select>
                     <Select name="size" onChange={handleFilters}>
                         <Option disabled >
